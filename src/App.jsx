62d0e67ec5121ceb93b92ba0554187c031ac92c3
Roberto Albergo <ralbergo7@gmail.com>
2025-06-08 12:12:25 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./Components/NavBar";
+import CardViaggio from "./Components/CardViaggio";
+function App() {
+  return (
+    <>
+      <NavBar />
+      <CardViaggio />
+      {/* <Container> */}
+    </>
+  );
+}
+
+export default App;
