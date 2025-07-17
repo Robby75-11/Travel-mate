1d5180f30cc62149145463478e4217eb36a0ee1b
Roberto Albergo <ralbergo7@gmail.com>
2025-07-17 11:59:30 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // verranno automaticamente reindirizzate a http://localhost:8080.
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080",
   headers: {
     "Content-Type": "application/json",
   },
